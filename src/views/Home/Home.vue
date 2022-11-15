e396d463d3cb4ac1cedbf0b9d435169525147029
Lightling <contactlightling@gmail.com>
2022-11-14 20:19:14 -0500

    Test CardEmbed

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,9 +1,57 @@
 <template lang='pug'>
-p Testing
+#main(
+)
+  CardEmbed(
+    v-for='card in cards'
+    :key='card.title'
+    :card='card'
+  )
 </template>
 
-<style lang='sass'>
+<style lang='sass' scoped>
+@import '@/styles/variables.sass'
+
+#main
+  position: absolute
+  top: 0
+  bottom: 0
+  left: 0
+  right: 0
+  display: flex
+  background-color: black
+  background-size: cover
+  background-position: center center
+  justify-content: center
+  align-items: center
+  .card
+    height: 80vh
+    width: 80vw
 </style>
 
 <script lang='ts'>
+import { defineComponent, ref } from 'vue'
+
+import { Card } from '@/types/card/Card'
+import { fetchAndParseContent } from '@/utilities/fetcher'
+import CardEmbed from '@/components/Card.vue'
+
+export default defineComponent({
+  components: {
+    CardEmbed,
+  },
+  setup () {
+    const cards = ref([] as Card[])
+
+    return {
+      cards
+    }
+  },
+  mounted () {
+    fetchAndParseContent('/content/info.yml')
+    .then((cards) => {
+      this.cards = cards as Card[]
+      console.log(this.cards)
+    })
+  },
+})
 </script>
